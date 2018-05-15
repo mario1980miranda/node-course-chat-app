691bc5a15d261ff191f0c13b87c5ab36e093ae3d
mario_chan15 <mario.luiz.miranda@gmail.com>
2018-05-14 21:39:13 -0300

    S09C115 - Add geolocation suppert via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -12,5 +12,18 @@ describe('generateMessage', () => {
         // expect(message.from).toBe('Mario');
         // expect(message.text).toBe('Mocha Test');
         expect(message).toInclude({from, text}); // ES6
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct message location object', () => {
+        var from = 'Admin';
+        var latitude = 1;
+        var longitude = 2;
+        var locationMessage = generateLocationMessage(from, latitude, longitude);
+        var url = 'https://www.google.com/maps?q=1,2';
+
+        expect(locationMessage.createdAt).toBeA('number');
+        expect(locationMessage).toInclude({from, url});
     });
 });
